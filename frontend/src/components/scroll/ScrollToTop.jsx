50e2b320e2b1b96ae7e31031aeefb8bac3c73c7b
kassem fawaz <kassemfawaz3@example.com>
2024-08-14 23:51:48 +0300

    new

--- frontend/src/components/scroll/ScrollToTop.jsx
+++ frontend/src/components/scroll/ScrollToTop.jsx
@@ -21,3 +21,33 @@ const ScrollToTop = () => {
 };
 
 export default ScrollToTop;
+
+
+
+// import { KeyboardArrowUp } from "@mui/icons-material";
+// import { Fab, Zoom, useScrollTrigger } from "@mui/material";
+
+// const ScrollToTop: React.FC = () => {
+//   const trigger = useScrollTrigger({ threshold: 100 });
+
+//   const handleClick = () => {
+//     window.scrollTo(0, 0);
+//   };
+
+//   return (
+//     <Zoom in={trigger}>
+//       <Fab
+//         onClick={handleClick}
+//         variant="extended"
+//         size="small"
+//         sx={{ position: "fixed", bottom: 33, right: 33 }}
+//         color="primary"
+//         aria-label="scroll to top"
+//       >
+//         <KeyboardArrowUp fontSize="medium" />
+//       </Fab>
+//     </Zoom>
+//   );
+// };
+
+// export default ScrollToTop;
